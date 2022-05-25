63064777d984f77cb421b97d1fc0e7025a7f34b5
dsyeag <16358063+dsyeag@users.noreply.github.com>
2022-05-25 10:10:45 -0400

    committing package.json
    Forgot to include dependency updates

--- contracts/treasurer.sol
+++ contracts/treasurer.sol
@@ -9,15 +9,15 @@
 
 pragma solidity ^0.8.0;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import {Math} from "@openzeppelin/contracts/math/Math.sol";
-import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+//import {Math} from "@openzeppelin/contracts/math/Math.sol";
+//import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract treasurer {
 
-    using Math for uint256;
-    using SafeMath for uint256;
+    //using Math for uint256;
+    //using SafeMath for uint256;
 
     address owner;    // current owner of the contract
 
