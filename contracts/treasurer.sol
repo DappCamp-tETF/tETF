9ea5263b7980203bbc0e8b565b0b1f00c26a63d5
dsyeag <16358063+dsyeag@users.noreply.github.com>
2022-05-27 18:44:57 -0400

    small debugging

--- contracts/treasurer.sol
+++ contracts/treasurer.sol
@@ -9,7 +9,7 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 //import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 //import {Math} from "@openzeppelin/contracts/math/Math.sol";
 //import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
@@ -21,7 +21,9 @@ contract treasurer {
     using SafeERC20 for IERC20;
 
     address owner;    // current owner of the contract
-
+    IERC20 usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    tokenAddress = web3.utils.toChecksumAddress(0x514910771af9ca656af840dff83e8264ecf986ca);
+    IERC20 tETF = IERC20(tokenAddress); //using LINK address as placeholder
     mapping (address => uint) usdcByAddress;
     mapping (address => uint) tETFByAddress;
 
@@ -32,19 +34,17 @@ contract treasurer {
     function withdrawUSDC(uint amount) public {
         require(owner == msg.sender);
         require(amount<=usdcByAddress[msg.sender],"You do not hold enough USDC");
-        IERC20 usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-        SafeERC20.safeTransferFrom(usdcAddress, address(this), msg.sender, amount);
+        SafeERC20.safeTransferFrom(usdc, address(this), msg.sender, amount);
         usdcByAddress[msg.sender]-=amount;
     }
     function withdrawtETF(uint amount) public {
-        require(amount<=tETFByAddress(msg.sender),"You do not hold enough tETF.");
-        SafeERC20.safeTransferFrom('tETFToken', address(this), msg.sender, amount);
+        require(amount<=tETFByAddress[msg.sender],"You do not hold enough tETF.");
+        SafeERC20.safeTransferFrom(tETF, address(this), msg.sender, amount);
         tETFByAddress[msg.sender]-=amount;
     }
     function deposit(address from, uint amount) public payable {
-        IERC20 token = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         require(msg.value == amount);
-        SafeERC20.safeTransferFrom(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, msg.sender, address(this), amount);
+        SafeERC20.safeTransferFrom(usdc, msg.sender, address(this), amount);
         usdcByAddress[from]+=amount;
     }
 
