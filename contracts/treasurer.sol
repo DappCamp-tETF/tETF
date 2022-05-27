44427354a3ce358860ea67920ebe29aaf3d9fc02
dsyeag <16358063+dsyeag@users.noreply.github.com>
2022-05-27 19:03:51 -0400

    think this fixes the OZ dependency issue

--- contracts/treasurer.sol
+++ contracts/treasurer.sol
@@ -9,7 +9,7 @@
 
 pragma solidity ^0.8.0;
 
-//import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 //import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 //import {Math} from "@openzeppelin/contracts/math/Math.sol";
 //import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
@@ -22,8 +22,8 @@ contract treasurer {
 
     address owner;    // current owner of the contract
     IERC20 usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    tokenAddress = web3.utils.toChecksumAddress(0x514910771af9ca656af840dff83e8264ecf986ca);
-    IERC20 tETF = IERC20(tokenAddress); //using LINK address as placeholder
+    //string tokenAddress = web3.utils.toChecksumAddress(0x514910771af9ca656af840dff83e8264ecf986ca);
+    IERC20 tETF = IERC20(0x514910771AF9Ca656af840dff83E8264EcF986CA); //using LINK address as placeholder
     mapping (address => uint) usdcByAddress;
     mapping (address => uint) tETFByAddress;
 
